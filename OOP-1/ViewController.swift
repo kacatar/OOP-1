6930aef72c269d7f0914f35767e86d9e05df2d0e
Helmi <helminizer@gmail.com>
2019-05-15 13:21:09 +0700

    Initial Commit

---
+++ OOP-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP-1
+//
+//  Created by Helmi on 15/05/19.
+//  Copyright © 2019 Helmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
